d9eacff5eb51a4b470d9a1ae86f1204264a6bae3
marut97 <marut1997@gmail.com>
2021-07-04 22:56:38 +0530

    Database implementation

---
+++ components/database.py
@@ -0,0 +1,48 @@
+import os
+import eyed3
+
+
+class Database:
+    def __init__(self):
+        self.path = os.getcwd() + "\\database"
+        self.songs = self.__read_songs()
+
+    def __read_songs(self):
+        songs = []
+        for file in os.listdir(self.path):
+            if file.endswith(".mp3"):
+                self.__load_song(os.path.join(self.path, file),  songs)
+        return songs
+
+    def __load_song(self, path, songs):
+        song = eyed3.load(path)
+        songs.append([path, song.tag.title, song.info.time_secs, song.tag.album,
+                      song.tag.artist, song.tag.getBestDate(), song.tag.genre])
+
+    def get_songs(self):
+        return self.songs
+
+    def get_song(self, index):
+        if index >= 0 and index < len(self.songs):
+            return self.songs[index]
+
+    def get_next_song(self, current_index):
+        if current_index + 1 < len(self.songs):
+            return self.songs[current_index + 1]
+        else:
+            return self.songs[0]
+
+    def get_previous_song(self, current_index):
+        if current_index - 1 >= 0:
+            return self.songs[current_index - 1]
+        else:
+            return self.songs[len(self.songs) - 1]
+
+
+if __name__ == "__main__":
+    db = Database()
+    print(db.get_songs())
+    print(db.get_song(0))
+    print(db.get_song(1))
+    print(db.get_next_song(0))
+    print(db.get_previous_song(0))
---
+++ music_player_application.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from ttkthemes import ThemedStyle
+
+
+class MusicPlayerApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        parent.title("Music Player")
+        parent.geometry("1000x600")
+
+        style = ThemedStyle(parent)
+        style.set_theme("scidgrey")
+
+
+if __name__ == "__main__":
+    window = tk.Tk()
+    MusicPlayerApplication(window).pack(side="top", fill="both", expand=True)
+    window.mainloop()
